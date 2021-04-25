d1ac6145e82c587dbb5090eddaf4017ee6bb809f
Neha Ghedia <nsghedia@gmail.com>
2021-04-25 09:48:05 -0700

    Upload Notice Layout

--- app/src/main/java/com/builddream/admin_svmit_college/MainActivity.java
+++ app/src/main/java/com/builddream/admin_svmit_college/MainActivity.java
@@ -1,14 +1,30 @@
 package com.builddream.admin_svmit_college;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    CardView cardView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        cardView = findViewById(R.id.addNotice);
+
+        cardView.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (cardView.getId()){
+            case R.id.addNotice:
+                Intent intent=new Intent(MainActivity.this,UploadNotice.class);
+                startActivity(intent);
+                break;
+        }
     }
 }
